a26ee1c8bd80d9832a364650cee6e52d2be8e8a1
Rousseau Alexandre <contact@rousseau-alexandre.fr>
2018-04-26 13:58:48 +0200

    Set my location

--- app/src/main/java/com/rousseau_alexandre/testmap/MainActivity.java
+++ app/src/main/java/com/rousseau_alexandre/testmap/MainActivity.java
@@ -1,16 +1,40 @@
 package com.rousseau_alexandre.testmap;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 
+import org.osmdroid.api.IMapController;
 import org.osmdroid.config.Configuration;
+import org.osmdroid.config.IConfigurationProvider;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
 
 public class MainActivity extends Activity {
-    MapView map = null;
+
+
+    private MapView map = null;
+    private final LocationListener locationListener = new MyLocationListener();
+
+    private static final long LOCATION_REFRESH_TIME = 1000;
+    private static final float LOCATION_REFRESH_DISTANCE = 10;
+
+    private static final int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 2812;
+
+
     @Override public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
@@ -18,7 +42,9 @@ public class MainActivity extends Activity {
 
         //load/initialize the osmdroid configuration, this can be done
         Context ctx = getApplicationContext();
-        Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
+        IConfigurationProvider configuration = Configuration.getInstance();
+
+        configuration.load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
         //setting this before the layout is inflated is a good idea
         //it 'should' ensure that the map has a writable location for the map cache, even without permissions
         //if no tiles are displayed, you can try overriding the cache path using Configuration.getInstance().setCachePath
@@ -30,6 +56,123 @@ public class MainActivity extends Activity {
 
         map = (MapView) findViewById(R.id.map);
         map.setTileSource(TileSourceFactory.MAPNIK);
+
+        // Built in Zoom control is required for emulator
+        // map.setBuiltInZoomControls(false);
+        map.setMultiTouchControls(true);
+
+        requestPermission();
+        setMyCurrentLocation();
+    }
+
+
+    /**
+     * @see https://developer.android.com/training/permissions/requesting
+     */
+    private void requestPermission() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Permission is not granted
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+            } else {
+                // No explanation needed; request the permission
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_READ_CONTACTS);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        } else {
+            // Permission has already been granted
+        }
+    }
+
+    /**
+     * @see https://developer.android.com/training/permissions/requesting
+     * @param requestCode
+     * @param permissions
+     * @param grantResults
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_READ_CONTACTS: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request.
+        }
+    }
+
+    private void setMyCurrentLocation() {
+        MyLocationNewOverlay mLocationOverlay = new MyLocationNewOverlay(new GpsMyLocationProvider(getApplicationContext()),map);
+        mLocationOverlay.enableMyLocation();
+        map.getOverlays().add(mLocationOverlay);
+
+        // Location loc = getLocation();
+
+        IMapController mapController = map.getController();
+        mapController.setZoom(17.0);
+        GeoPoint startPoint = new GeoPoint(48.8583, 2.2944);
+        //mapController.setCenter(startPoint);
+    }
+
+    private Location getLocation() {
+        requestPermission();
+
+        Context context = getApplicationContext();
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        boolean locationServiceAvailable = false;
+
+        // check location available for API >= 23
+        if(Build.VERSION.SDK_INT >= 23){
+            locationServiceAvailable = ContextCompat.checkSelfPermission( context, android.Manifest.permission.ACCESS_FINE_LOCATION ) != PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission( context, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED;
+        }
+
+        boolean isGPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        boolean isNetworkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+        String provider;
+        if(isGPSEnabled) {
+            provider = LocationManager.GPS_PROVIDER;
+        }else if(isNetworkEnabled) {
+            provider = LocationManager.NETWORK_PROVIDER;
+        }else{
+            return null;
+        }
+
+        locationManager.requestLocationUpdates(
+                provider,
+                LOCATION_REFRESH_TIME,
+                LOCATION_REFRESH_DISTANCE,
+                this.locationListener
+        );
+
+        if (locationManager != null)  {
+            Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        }
+
+        return null;
     }
 
     public void onResume(){
